2325e3d6d09c2bb84f75f4fe323a462d92b4666f
angehl <angehl@gmail.com>
2019-08-07 12:48:01 +0000

    Empezando ml

---
+++ EventTracer/EventTracer.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Permissions;
+using Newtonsoft.Json;
+using System.Xml;
+
+using Microsoft.Diagnostics.Symbols;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using System.Threading;
+using System.IO;
+
+namespace EventTracer
+{
+    class Tracingfromfile
+    {
+        static List<int> tracking; //Process being tracked
+        static TraceEventSession kernelSession; //Session
+        static Process baseProcess; //This will execute the file
+        static string dumpfile; //Output file, JSON format
+        static XmlDocument d;   //XmlDocument that will access to data
+
+        static void Main(string[] args)
+        {
+            //Check admin privileges
+            if (TraceEventSession.IsElevated() == false)
+            {
+                Console.WriteLine("Please run me as Administrator");
+                Console.ReadLine();
+                return;
+            }
+
+            //Get the file to analyze
+            string filePath = string.Empty;
+            Console.WriteLine("File to analyze");
+            filePath = Console.ReadLine();
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("The input file {0} does not exists", filePath);
+                Console.ReadLine();
+                return;
+            }
+
+            //Get output file
+            Console.WriteLine("Output file");
+            dumpfile = Console.ReadLine();
+            try
+            {
+                System.IO.File.WriteAllText(dumpfile, string.Empty);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Unable to open Output File: {0}", e);
+                return;
+            }
+
+            Tracingfromfile tracer = new Tracingfromfile(filePath);
+            tracer.begin();
+        }
+
+        //Start the tracking
+        public void begin()
+        {
+
+            try
+            {
+                ThreadStart ths = new ThreadStart(() =>
+                {
+                    Thread.Sleep(1000);
+                    baseProcess.Start();
+                    Console.WriteLine("Starting process");
+                    Tracingfromfile.tracking.Add(baseProcess.Id); //Starts tracking the process we created
+                });
+                Thread th = new Thread(ths);
+                th.Start();
+                kernelSession.Source.Process(); //Starts the tracer
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //Builder
+        public Tracingfromfile(string filename)
+        {
+
+            tracking = new List<int>();
+            d = new XmlDocument();
+            //Initializing ETW session
+            kernelSession = new TraceEventSession("ChaChaRealSmooth");
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e) { kernelSession.Dispose(); };
+
+            //subscribe to kernel events
+            kernelSession.EnableKernelProvider(KernelTraceEventParser.Keywords.All);
+
+            //Setup function handlers
+            kernelSession.Source.Kernel.ProcessStart += processStarted;
+            kernelSession.Source.Kernel.ProcessStop += processStopped;
+            kernelSession.Source.Kernel.UdpIpRecv += general;
+            kernelSession.Source.Kernel.UdpIpSend += general;
+            //kernelSession.Source.Kernel.UdpIpFail += general;
+            kernelSession.Source.Kernel.ImageLoadGroup += general;
+            kernelSession.Source.Kernel.ImageUnloadGroup += general;
+            kernelSession.Source.Kernel.SystemConfigNIC += general;
+            kernelSession.Source.Kernel.SystemConfigVideo += general;
+            kernelSession.Source.Kernel.SystemConfigServices += general;
+            kernelSession.Source.Kernel.SystemConfigPower += general;
+            kernelSession.Source.Kernel.SystemConfigIRQ += general;
+            kernelSession.Source.Kernel.SystemConfigPnP += general;
+            kernelSession.Source.Kernel.SystemConfigNetwork += general;
+            kernelSession.Source.Kernel.SystemConfigIDEChannel += general;
+            kernelSession.Source.Kernel.SystemConfigLogDisk += general;
+            kernelSession.Source.Kernel.SystemConfigPhyDisk += general;
+            //kernelSession.Source.Kernel.TcpIpConnectIPV6 += general;
+            //kernelSession.Source.Kernel.TcpIpDisconnectIPV6 += general;
+            kernelSession.Source.Kernel.PerfInfoISR += general;
+            kernelSession.Source.Kernel.PerfInfoDPC += general;
+            kernelSession.Source.Kernel.SystemConfigCPU += general;
+            kernelSession.Source.Kernel.DiskIORead += general;
+            kernelSession.Source.Kernel.DiskIOWrite += general;
+            kernelSession.Source.Kernel.DiskIODriverMajorFunctionCall += general;
+            kernelSession.Source.Kernel.DiskIODriverMajorFunctionReturn += general;
+            kernelSession.Source.Kernel.ThreadStartGroup += general;
+            kernelSession.Source.Kernel.ThreadEndGroup += general;
+            kernelSession.Source.Kernel.TcpIpSend += general;
+            kernelSession.Source.Kernel.TcpIpRecv += general;
+            //kernelSession.Source.Kernel.TcpIpDisconnect += general;
+            //kernelSession.Source.Kernel.TcpIpConnect += general;
+            kernelSession.Source.Kernel.TcpIpSendIPV6 += general;
+            kernelSession.Source.Kernel.TcpIpRecvIPV6 += general;
+            //kernelSession.Source.Kernel.FileIOMapFile += general;
+            //kernelSession.Source.Kernel.FileIOUnmapFile += general;
+            //kernelSession.Source.Kernel.FileIOWrite += general;
+            //kernelSession.Source.Kernel.FileIORead += general;
+            kernelSession.Source.Kernel.FileIOFileCreate += general;
+            kernelSession.Source.Kernel.FileIOFileDelete += general;
+            //kernelSession.Source.Kernel.All += general;
+
+            //setting up the staring process
+            baseProcess = new Process();
+            baseProcess.StartInfo.UseShellExecute = true;
+            baseProcess.StartInfo.FileName = filename;
+            baseProcess.StartInfo.CreateNoWindow = false;
+        }
+
+        private static void general(TraceEvent data)
+        {
+            if (Tracingfromfile.tracking.Contains(data.ProcessID))
+            {
+                Tracingfromfile.d.LoadXml(data.Dump());
+                File.AppendAllText(@Tracingfromfile.dumpfile,JsonConvert.SerializeXmlNode(d)+ "\n");
+            }
+        }
+
+       
+        private static void processStarted(ProcessTraceData data)
+        {
+            if (Tracingfromfile.tracking.Contains(data.ProcessID))
+            {
+                Tracingfromfile.d.LoadXml(data.Dump());
+                File.AppendAllText(@Tracingfromfile.dumpfile, JsonConvert.SerializeXmlNode(d) + "\n");
+            }
+            else if (Tracingfromfile.tracking.Contains(data.ParentID))
+            {
+                Tracingfromfile.tracking.Add(data.ProcessID);
+                Console.WriteLine("Remaining process: {0}", Tracingfromfile.tracking.Count());
+                Tracingfromfile.d.LoadXml(data.Dump());
+                File.AppendAllText(@Tracingfromfile.dumpfile, "{\"ID\":\"" + data.ProcessID + "\",\"Parent\":\"" + data.ParentID + ",\"Type\":\"ProcessStarted\",\"Payload\"" + JsonConvert.SerializeXmlNode(d) + "}");
+            }
+
+        }
+
+        private static void processStopped(ProcessTraceData data)
+        {
+            if (Tracingfromfile.tracking.Contains(data.ProcessID))
+            {
+                Tracingfromfile.d.LoadXml(data.Dump());
+                File.AppendAllText(@Tracingfromfile.dumpfile, JsonConvert.SerializeXmlNode(d) + "\n");
+                Tracingfromfile.tracking.Remove(data.ProcessID);
+                Console.WriteLine("Remaining process: {0}", Tracingfromfile.tracking.Count());
+                if (Tracingfromfile.tracking.Count() == 0)
+                {
+                    Console.WriteLine("Process Finished");
+                }
+            }
+        }
+
+    }
+}
---
+++ MachineLearning/Network.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NeuralNetworkNET;
+using NeuralNetworkNET.SupervisedLearning;
+using System.Threading.Tasks;
+using NeuralNetworkNET.APIs;
+using NeuralNetworkNET.APIs.Datasets;
+using NeuralNetworkNET.APIs.Enums;
+using NeuralNetworkNET.APIs.Interfaces;
+using NeuralNetworkNET.APIs.Interfaces.Data;
+using NeuralNetworkNET.APIs.Results;
+using NeuralNetworkNET.APIs.Structs;
+using NeuralNetworkNET.Helpers;
+using NeuralNetworkNET.SupervisedLearning.Progress;
+using NeuralNetworkNET.Networks;
+using NeuralNetworkNET.Networks.Graph;
+using NeuralNetworkNET.Networks.Activations;
+
+
+namespace MachineLearning
+{
+    class NNetwork
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            INeuralNetwork network = NetworkManager.NewGraph(TensorInfo.Linear(5), root =>
+                var input1 = root.Layer(CuDnnNetworkLayers.FullyConnected
+            );
+        }
+    }
+}
